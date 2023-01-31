d42cc71537fcde9aa960bcf7eb5e8a5c59bee780
KRO5RT <robert.kohn@bosch.com>
2023-01-31 11:02:20 +0100

    Initial commit

---
+++ src/main/java/com/treboder/Arc42DocuExample/Arc42DocuExampleApplication.java
@@ -0,0 +1,13 @@
+package com.treboder.Arc42DocuExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arc42DocuExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arc42DocuExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treboder/Arc42DocuExample/Arc42DocuExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treboder.Arc42DocuExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arc42DocuExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
